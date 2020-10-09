e8068ccebbdcacb85e3d72f7b8403afab308c3e3
gnharishkumar13 <harishkumar_gn@yahoo.com>
2020-10-09 19:11:04 -0400

    changed order

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,9 +6,9 @@ const Card = ({id, email, name }) => {
         <div className='tc bg-light-green dib br3 pa2 ma2 grow bw2 shadow-5'>
             <img src={`https://robohash.org/${id}?200x200`} alt='robots' />
             <div>
-                <h2>{id}</h2>
-                <p>{email}</p>
                 <p>{name}</p>
+                <p>{email}</p>
+
             </div>
         </div>
     );
